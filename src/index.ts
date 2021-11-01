99f090c275c472de42cd4d1550b96a331c47f709
ismail harmanda <iharmanda@hotmail.com>
2021-11-01 12:51:16 +0300

    Sorter Scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const logSomething = () => {
-  console.log("Hi there");
-};
+class Sorter {
+  constructor(public collection: number[]) {}
 
-logSomething();
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
