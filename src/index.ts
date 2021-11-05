deac2713776635aacd62639c13437d48af6ee3e8
ismail harmanda <iharmanda@hotmail.com>
2021-11-05 17:39:24 +0300

    END

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,18 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, -8]);
-
 numbersCollection.sort();
-
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection("İsmail");
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection("İsmail");
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
 
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+linkedList.sort();
+linkedList.print();
