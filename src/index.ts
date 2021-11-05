f1204a6877f7d76842c7d07d5068c8ea0e04cab8
ismail harmanda <iharmanda@hotmail.com>
2021-11-05 17:32:30 +0300

    Sorting NumbersCollection done.

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,23 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0, -8]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(sorter.collection);
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, -8]);
+
+numbersCollection.sort();
+
+console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection("İsmail");
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 // console.log(charactersCollection.data);
 
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
-linkedList.print();
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
